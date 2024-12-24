99c1263675c4356141611c9210d1967b69e4b681
karthikeyan13762 <karthikeya13762@gmail.com>
2024-12-24 15:42:52 +0530

    frontend deployment added inbacked for cors

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const app = express();
 //
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://library-management-system-8.netlify.app",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 dotenv.config();
 
