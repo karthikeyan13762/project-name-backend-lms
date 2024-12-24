c5bbd80ee1aa492a8e63ee68fc9293cdb8aa1bc0
karthikeyan13762 <karthikeya13762@gmail.com>
2024-12-24 22:10:26 +0530

    fixing add book  and lodut  errors

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ import { Book } from "./models/Book.js";
 const app = express();
 //
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://library-management-system-8.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // If you're using cookies
+  next();
+});
 app.use(
   cors({
-    origin: "https://library-management-system-8.netlify.app",
-    credentials: true,
+    origin: "https://library-management-system-8.netlify.app", // Your frontend URL
+    credentials: true, // Allow cookies to be sent
   })
 );
 app.use(cookieParser());
--- routes/book.js
+++ routes/book.js
@@ -4,33 +4,33 @@ import { Book } from "../models/Book.js";
 
 import { verifyAdmin } from "./auth.js";
 
-// import multer from "multer";
-// // Configure Multer for file uploads
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/"); // Save files in the "uploads" directory
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname); // Add timestamp to ensure unique filenames
-//   },
-// });
-// const upload = multer({ storage });
-
 import multer from "multer";
-import path from "path";
-
+// Configure Multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = path.join(__dirname, "uploads");
-    cb(null, uploadDir);
+    cb(null, "uploads/"); // Save files in the "uploads" directory
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, `${uniqueSuffix}-${file.originalname}`);
+    cb(null, Date.now() + "-" + file.originalname); // Add timestamp to ensure unique filenames
   },
 });
-
 const upload = multer({ storage });
+
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     const uploadDir = path.join(__dirname, "uploads");
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, `${uniqueSuffix}-${file.originalname}`);
+//   },
+// });
+
+// const upload = multer({ storage });
 
 const router = expres.Router();
 // Add Book Endpoint
